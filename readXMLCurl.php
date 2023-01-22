72d60de681be5fdb18160a462405d6fbc499f136
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-22 18:05:08 +0100

    Création des ordres de création de la table des pilotes

--- readXMLCurl.php
+++ readXMLCurl.php
@@ -12,20 +12,76 @@
 
     // Initialisation du premier attribut (erreur dans les données restituées)
     foreach ($xml->DriverTable->attributes() as $attribute => $value) {
-        echo $attribute, ' : ', $value, "<br>";
+        echo "Pilotes . <br><br>";
     }
 
+    // Création de la table
+    $sql_create = 'CREATE TABLE driver (';
+    $i = 0;
     foreach ($xml->DriverTable as $drivers) {
+        foreach ($drivers as $driver) {
+            $i++;
+            // attributs
+            foreach ($driver->attributes() as $attribute => $value) {
+                $sql_create .= $attribute . ' VARCHAR(255), ';
+                $columns .= $attribute . ', ';
+            }
+
+            // éléments du tableau
+            $attributes = substr($attributes, 0, -2);
+            foreach ($driver as $key => $value) {
+                $sql_create .= $key . ' VARCHAR(255), ';
+                $columns .= $key . ', ';
+            }
+            break 1;
+        }
+    }
+    $sql_create = substr($sql_create, 0, -2) .  ')';
+    $columns = substr($columns, 0, -2);
+
+    echo $sql_create;
+
+    echo "<br><br>";
+
+    // Création des enregistrements
+    foreach ($xml->DriverTable as $drivers) {
+        foreach ($drivers as $driver) {
+            $sql_insert = 'INSERT into driver (' . $columns . ') VALUES (';
+
+            // attributs
+            foreach ($driver->attributes() as $attribute => $value) {
+                $sql_insert .= '\'' . $value . '\'' . ', ';
+            }
+
+            // éléments du tableau
+            foreach ($driver as $key => $value) {
+                $sql_insert .= '\'' . $value . '\'' . ', ';
+            }
+            $sql_insert = substr($sql_insert, 0, -2) .  ')';
+            echo $sql_insert;
+            $sql_insert = '';
+            echo "<br><br>";
+        }
+    }
+
+
+    // Affichage des données
+    foreach ($xml->DriverTable as $drivers) {
+        $sql_insert = 'INSERT into driver (' . $columns . ') VALUES (';
         foreach ($drivers as $driver) {
             // attributs
             foreach ($driver->attributes() as $attribute => $value) {
+                $sql_insert .= '\'' . $value . '\'' . ', ';
                 echo $attribute, ' : ', $value, "<br>";
             }
 
             // éléments du tableau
             foreach ($driver as $key => $value) {
+                $sql_insert .= '\'' . $value . '\'' . ', ';
                 echo $key . " : " . $value . "<br>";
             }
-            echo "<br>";
+            $sql_insert = substr($sql_insert, 0, -2) .  ')';
+            echo $sql_insert;
+            echo "<br><br>";
         }
     }
