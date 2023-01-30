f64548c5b13a91b0b8dc1676532ebd9acac51d38
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-30 20:53:32 +0100

    Mise en place de templates avec Twig

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo "Page d'accueil <br><br>";
+include 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('driver/list.html.twig');
