dd085ff9d662e1adc33b9667fbea5730ff59ab33
François Bonnafous <francois.bonnafous@hotmail.fr>
2023-01-31 08:41:27 +0100

    Affichage du tableau des pilotes

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Utils\Database\GetDatabase;
+
 include 'vendor/autoload.php';
+
+require_once('libraries/autoload.php');
 
 $loader = new \Twig\Loader\FilesystemLoader('./templates');
 
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('driver/list.html.twig');
+$db = GetDatabase::getDatabase();
+
+$drivers_list = $db->query("SELECT * FROM driver WHERE season = 2022");
+
+echo $twig->render('driver/list.html.twig', [
+    'drivers' => $drivers_list
+]);
